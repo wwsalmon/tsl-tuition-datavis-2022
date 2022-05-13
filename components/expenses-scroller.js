0cfc4bda51382013d6ad093bd06e27c1442810d5
Samson Zhang <wwsamson@yahoo.com>
2022-05-13 15:29:44 -0400

    implement animation on scroll

--- components/expenses-scroller.js
+++ components/expenses-scroller.js
@@ -21,33 +21,43 @@ const expData = Object.keys(allData).map(d => {
 
 console.log(expData);
 
-const width = 1920;
-const height = 1080;
+const width = 960;
+const height = 540;
 
-function step1Chart(container, field, graphWidth, graphHeight) {
-    const yScale = d3.scaleLinear().domain([0, d3.max(expData.map(d => d[field]))]).range([graphHeight, 0]);
-    const xScale = d3.scaleBand().range([0, graphWidth]).domain(expData.map(d => d.school)).padding(0.2);
-    const bandwidth = xScale.bandwidth();
+const pl = 48;
+const pr = 48;
+const pb = 48;
+const pt = 48;
+const graphWidth = width - pl - pr;
+const graphHeight = height - pt - pb;
 
-    const splitData = expData.map(d => {
-        const startHeight = yScale(d.expenses);
-        const barHeight = graphHeight - startHeight;
-        const numShortSide = Math.round(Math.sqrt(d.enrollment / (barHeight / bandwidth)));
-        return {
-            startHeight,
-            school: d.school,
-            barHeight,
-            squareSide: bandwidth / numShortSide,
-            numShortSide,
-            numLongSide: Math.round(numShortSide * barHeight / bandwidth),
-        };
-    });
+const yScale = d3.scaleLinear().domain([0, d3.max(expData.map(d => d.expenses))]).range([graphHeight, 0]);
+const xScale = d3.scaleBand().range([0, graphWidth]).domain(expData.map(d => d.school)).padding(0.2);
+const bandwidth = xScale.bandwidth();
+
+const splitData = expData.map(d => {
+    const startHeight = yScale(d.expenses);
+    const barHeight = graphHeight - startHeight;
+    const numShortSide = Math.round(Math.sqrt(d.enrollment / (barHeight / bandwidth)));
+    return {
+        startHeight,
+        school: d.school,
+        barHeight,
+        squareSide: bandwidth / numShortSide,
+        numShortSide,
+        numLongSide: Math.round(numShortSide * barHeight / bandwidth),
+    };
+});
+
+function initialize(svg) {
+    const container = svg.append("g").style("transform", `translate(${pl}px, ${pt}px)`).attr("id", "container");
 
     const barGroups = container
         .selectAll("g.barGroups")
         .data(splitData)
         .enter()
         .append("g")
+        .attr("class", "barGroup")
         .style("transform", d => `translate(${xScale(d.school)}px, ${d.startHeight}px)`);
 
     barGroups
@@ -65,79 +75,130 @@ function step1Chart(container, field, graphWidth, graphHeight) {
         .attr("fill", d => getColor(schoolLabels[d.school]));
 
     barGroups
-        .selectAll("path.vertSplit")
+        .selectAll("path.splitLineY")
         .data(d => Array(d.numShortSide).fill(d.squareSide))
         .enter()
         .append("path")
-        .attr("class", "vertSplit")
-        .attr("d", (d,i) => d3.line()([[i * d, 0], [i * d, graphHeight]]))
-        .attr("stroke", "white")
-        .attr("strokeWidth", 1);
+        .attr("class", "splitLineY splitLine")
+        .attr("d", (d,i) => d3.line()([[i * d, 0], [i * d, height]]));
 
     barGroups
-        .selectAll("path.horzSplit")
+        .selectAll("path.splitLineX")
         .data(d => Array(d.numLongSide).fill(d.squareSide))
         .enter()
         .append("path")
-        .attr("class", "horzSplit")
-        .attr("d", (d,i) => d3.line()([[0, i * d], [bandwidth, i * d]]))
-        .attr("stroke", "white")
-        .attr("strokeWidth", 1);
+        .attr("class", "splitLineX splitLine")
+        .attr("d", (d,i) => d3.line()([[0, i * d], [bandwidth, i * d]]));
 
-    container.selectAll(".horzSplit, .vertSplit")
-        .style("opacity", 0)
-        .transition()
-        .delay(1000)
-        .duration(750)
-        .style("opacity", 1)
-        .transition()
-        .delay(1000)
-        .duration(750)
+    container
+        .selectAll(".splitLine")
+        .attr("stroke", "white")
+        .attr("strokeWidth", 1)
         .style("opacity", 0);
 
     container.append("g").attr("class", "xAxis").call(d3.axisBottom(xScale).tickSize(0)).style("transform", `translateY(${graphHeight}px)`);
+}
 
-    container.append("g").attr("class", "yAxis").call(d3.axisLeft(yScale));
+function step2From1(svg) {
+    console.log("2 from 1");
 
-    container.selectAll(".xAxis, .yAxis")
+    const container = svg.select("#container");
+    const barGroups = svg.selectAll(".barGroup");
+
+    console.log(container, barGroups);
+
+    // split lines in (t1)
+    container.selectAll(".splitLine")
         .transition()
-        .delay(1000)
+        .duration(750)
+        .style("opacity", 1);
+
+    // axes out (t1)
+    container.selectAll(".xAxis")
+        .transition()
         .duration(750)
         .style("opacity", 0);
 
-    container.selectAll(".singleRect")
-        .transition()
-        .delay(4250)
-        .duration(750)
-        .style("transform", "scale(20)");
-
-    container.selectAll(".mainRect")
-        .transition()
-        .delay(2750)
-        .duration(750)
-        .style("opacity", 0);
-
+    // barGroups align top (t1)
     barGroups
+        .data(splitData)
+        .join("g")
         .transition()
-        .delay(3500)
         .duration(750)
         .style("transform", d => `translate(${xScale(d.school)}px, 0px)`);
+
+    // main rects out (t2)
+    container.selectAll(".mainRect")
+        .transition()
+        .delay(750)
+        .duration(750)
+        .style("opacity", 0);
+
+    // split lines out (t2)
+    container.selectAll(".splitLine")
+        .transition()
+        .delay(750)
+        .duration(750)
+        .style("opacity", 0);
+
+    // single rects big (t3)
+    container.selectAll(".singleRect")
+        .transition()
+        .delay(1500)
+        .duration(750)
+        .style("transform", "scale(24)");
 }
 
-function step1On(svg) {
-    const graphHeight = 800;
-    const pl = 48;
-    const pr = 48;
-    const pb = 48;
-    const graphWidth = width - pl - pr;
-    const totalHeight = graphHeight + pb;
+function step1From2(svg) {
+    console.log("1 from 2");
 
-    const transformY = (height - totalHeight) / 2;
-    const expensesChart = svg.append("g").style("transform", `translate(${pl}px, ${transformY}px)`);
-    const expensesPerStudentChart = svg.append("g").style("transform", `translate(${width / 2 + pl}px, ${transformY}px)`);
+    const container = svg.select("#container");
+    const barGroups = svg.selectAll(".barGroup");
 
-    step1Chart(expensesChart, "expenses", graphWidth, graphHeight);
+    // single rects small (t1)
+    container.selectAll(".singleRect")
+        .transition()
+        .duration(750)
+        .style("transform", "scale(1)");
+
+    // split lines in (t2)
+    container.selectAll(".splitLine")
+        .transition()
+        .delay(750)
+        .duration(750)
+        .style("opacity", 1);
+
+    // main rects in (t2)
+    container.selectAll(".mainRect")
+        .transition()
+        .delay(750)
+        .duration(750)
+        .style("opacity", 1);
+
+    // barGroups to position (t3)
+    barGroups
+        .data(splitData)
+        .join("g")
+        .transition()
+        .delay(1500)
+        .duration(750)
+        .style("transform", d => `translate(${xScale(d.school)}px, ${d.startHeight}px)`);
+
+    // axes in (t3)
+    container.selectAll(".xAxis")
+        .transition()
+        .delay(1500)
+        .duration(750)
+        .style("opacity", 1);
+
+    // split lines out (t3)
+    container.selectAll(".splitLine")
+        .transition()
+        .delay(1500)
+        .duration(750)
+        .style("opacity", 0);
 }
+
 
 class ExpensesScroller extends D3Component {
     initialize(node, props) {
@@ -145,31 +206,19 @@ class ExpensesScroller extends D3Component {
 
         const svg = (this.svg = d3.select(node).append("svg"));
 
-        step1On(svg);
-
         svg
             .attr("viewBox", `0 0 ${width} ${height}`)
             .style("width", "100%")
             .style("height", "100vh");
 
-        svg
-            .append('circle')
-            .attr('r', 20)
-            .attr('cx', Math.random() * width)
-            .attr('cy', Math.random() * height);
+        initialize(svg);
     }
 
     update(props, oldProps) {
         const {step} = props;
 
-        console.log(step);
-
-        this.svg
-            .selectAll('circle')
-            .transition()
-            .duration(750)
-            .attr('cx', Math.random() * width)
-            .attr('cy', Math.random() * height);
+        if (step === 0) return step1From2(this.svg);
+        if (step === 1) return step2From1(this.svg);
     }
 }
 
