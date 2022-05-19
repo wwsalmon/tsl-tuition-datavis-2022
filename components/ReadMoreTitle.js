db5d524870b2a2f25894abaea854a10f9e0d5305
Samson Zhang <wwsamson@yahoo.com>
2022-05-19 02:18:00 -0400

    wip

--- components/ReadMoreTitle.js
+++ components/ReadMoreTitle.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function ReadMoreTitle() {
     return (
-        <div style={{textTransform: "uppercase", fontWeight: 700, marginTop: 64, fontSize: 16, textAlign: "center", opacity: 0.5}}><span>Read more</span></div>
+        <div style={{textTransform: "uppercase", fontWeight: 700, fontSize: 16, textAlign: "center", opacity: 0.5}}><span>Read more</span></div>
     )
 }
 
